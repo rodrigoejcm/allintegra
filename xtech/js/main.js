3e5dc8888547997ec89bd9497e5f4d40ee3299b2
Rodrigo <rodrigo.ejcm@gmail.com>
2017-02-13 16:07:21 -0200

    backup allintegra

--- xtech/js/main.js
+++ xtech/js/main.js
@@ -1,8 +1,4 @@
-/*
-	Spectral by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+
 
 (function($) {
 
@@ -18,22 +14,6 @@
 			$como_funciona = $('#banner_como_funciona');
 		
 
-		// Disable animations/transitions until the page has loaded.
-
-
-		// Mobile?
-			/*if (skel.vars.mobile)
-				$body.addClass('is-mobile');
-			else
-				skel
-					.on('-medium !medium', function() {
-						$body.removeClass('is-mobile');
-					})
-					.on('+medium', function() {
-						$body.addClass('is-mobile');
-					});
-*/
-		
 
 		// Scrolly.
 			$('.scrolly')
